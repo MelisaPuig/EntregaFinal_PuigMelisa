8dd230a446763c45ed95125df38896264e6e1391
Melisa Puig <mel.puig@gmail.com>
2022-03-04 01:32:03 -0300

    ultima entrega

---
+++ pages/js/main.js
@@ -0,0 +1,12 @@
+
+$("#boton-submit").on("click", ()=> {  
+    window.location.href = "../pages/error.html";
+    return false;
+}
+);
+
+$(`#buttonReturnHome`).on("click", ()=> {  
+    window.location.href = "/index.html";
+    return false;
+    }
+    );
